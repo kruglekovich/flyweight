0cbbedb1a9cbbb4cb1a8cd1627498b860cb783eb
n.xarlov.2020 <n.xarlov.2020@CLOUD-RDHOST1>
2023-04-07 14:30:47 +0700

    Добавьте файлы проекта.

---
+++ flyweight/Tree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flyweight
+{
+    public class Tree
+    {
+        public void Display(double x, double y, int age)
+        {
+            Console.WriteLine("Drawing a {0} year old tree at x = {1}");
+        }
+    }
+}
